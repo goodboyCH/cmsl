1022c859edf1ec80dd527b44ffeac5b6c5ee9b9c
CH <hch0671@kookmin.ac.kr>
2025-10-22 17:05:22 +0900

    feat: professor page

--- src/components/pages/ProfessorPage.tsx
+++ src/components/pages/ProfessorPage.tsx
@@ -62,7 +62,7 @@ export function ProfessorPage() {
           <img 
             src={content.profile_image_url} 
             alt={content.name} 
-            className="w-62 h-86 object-cover rounded-lg shadow-md flex-shrink-0"
+            className="w-64 h-80 object-cover rounded-lg shadow-md flex-shrink-0"
           />
           <div className="flex-1 space-y-4 w-full">
             <h1 className="text-4xl font-bold text-gray-800 dark:text-white">{content.name}</h1>
