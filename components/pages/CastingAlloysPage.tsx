f1a46ecb2e81c14224ddc68292077459b9149ac3
이총명 <dlchdaud04@naver.com>
2025-12-30 15:25:59 +0900

    Squashed commit of the following:
    commit c289c63180ef9f47331410d87d0ca00c2bb6c266
    Merge: 1e41c2c 4118a27
    Author: goodboyCH <hch0671@naver.com>
    Date:   Tue Dec 30 15:14:13 2025 +0900
    
        Merge pull request #1 from goodboyCH/feature/rtp
    
        토픽 페이지 수정
    
    commit 4118a275f9745726c39205cc35de37bfb30ad048
    Author: goodboyCH <hch0671@naver.com>
    Date:   Tue Dec 30 15:11:06 2025 +0900
    
        토픽 페이지 수정

--- components/pages/CastingAlloysPage.tsx
+++ components/pages/CastingAlloysPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ResearchPageTemplate, ResearchPageContent } from '@/components/ResearchPageTemplate';
 
 const defaultContent: Partial<ResearchPageContent> = {
-  title: "Casting Alloys Research",
-  subtitle: "Pioneering the future of high-strength, lightweight casting alloys for automotive and aerospace applications.",
+  title: "Real Scale PFM Research",
+  subtitle: "Pioneering the future of high-strength, lightweight Real Scale PFM for automotive and aerospace applications.",
   representative_media: { url: '', type: 'image' },
   research_sections: [],
   related_publications_title: "Key Publications",
